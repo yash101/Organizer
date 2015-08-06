bb7221bef17adeaf191d225fb4b596b2dc8283f2
dev lodha <dlodha@pvlearners.net>
2015-08-05 23:26:04 -0700

    Just updated some documentation

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 (function()
 {
+  //Import express JS and set up our app
   var express = require("express");
   var app = express();
 
+  //Enable the server and begin listening on port 1234
   var server = app.listen(1234, function()
   {
     var host = server.address().address;
     var port = server.address().port;
-
-    console.log("Server stared listening at: http://%s:%s", host, port);
   });
 
+  //We want to have the resources directory be statically available
   app.use(express.static("resources"));
 
+  //Return the index page as the fallback.
   app.get("*", function(req, res)
   {
     res.sendFile(__dirname + "/resources/index.html");
